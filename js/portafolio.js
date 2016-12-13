3f449f2e4a1ba3c74016702e2d4c5ba1f82341dc
kellyurribarr <kellyurribarri@gmail.com>
2016-12-13 10:06:01 -0300

    actualizacion

--- js/portafolio.js
+++ js/portafolio.js
@@ -42,3 +42,18 @@ $(document).ready(function(){
 
 //==============  
 
+$('.arriba').click(function(){
+        $('body,html').animate({
+            scrollTop: '0px'
+        }, 1000)
+    })
+
+    $(window).scroll(function(){
+
+        if ($(this).scrollTop() > 0){
+            $('.arriba').slideDown(100);
+        }else {
+            $('.arriba').slideUp(100);
+        }
+    });
+
